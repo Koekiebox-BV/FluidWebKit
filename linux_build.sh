4374a9c980df6f5f2ac557b67e6e6891a3b65781
koekiebox <jason@koekiebox.com>
2020-08-13 23:01:30 +0200

    Bump core for Kit.

--- linux_build.sh
+++ linux_build.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
-mvn clean && mvn clean install -U
+mvn clean && mvn clean install -U -Dgpg.skip
 
 echo '[INFO] Updating jar in FlowJob'
-rm -rfv /Users/jasonbruwer/Workspace/FlowJob/flow-job-setup/docker/koekiebox/fluid_base/external_lib/com/fluidwebkit/fluidwebkit-*.jar
-cp target/fluidwebkit-*.jar /Users/jasonbruwer/Workspace/FlowJob/flow-job-setup/docker/koekiebox/fluid_base/external_lib/com/fluidbpmwebkit
 
-rm -rfv /Users/jasonbruwer/Workspace/FlowJob/flow-job-setup/docker/koekiebox/fluid_base/external_lib/com/fluidwebkit/fluidwebkit-*-sources.jar
-rm -rfv /Users/jasonbruwer/Workspace/FlowJob/flow-job-setup/docker/koekiebox/fluid_base/external_lib/com/fluidwebkit/fluidwebkit-*-javadoc.jar
+#PROPERTIES
+export WORKSPACE_HOME='/home/jbruwer/GoogleDrive/Workspace'
+export WF_HOME='/home/jbruwer/Applications/wildfly-20.0.1.Final'
 
-echo '[INFO] Updating WildFly-17'
-cp -fv /Users/jasonbruwer/Workspace/FlowJob/flow-job-setup/docker/koekiebox/fluid_base/external_lib/com/fluidbpmwebkit/fluidwebkit-*.jar /Users/jasonbruwer/Applications/wildfly-17.0.0.Final/modules/com/fluidbpmwebkit/main/
+rm -rfv $WORKSPACE_HOME/FlowJob/flow-job-setup/docker/koekiebox/fluid_base/external_lib/com/fluidwebkit/fluidwebkit-*.jar
+cp target/fluidwebkit-*.jar $WORKSPACE_HOME/FlowJob/flow-job-setup/docker/koekiebox/fluid_base/external_lib/com/fluidbpmwebkit
+
+rm -rfv $WORKSPACE_HOME/FlowJob/flow-job-setup/docker/koekiebox/fluid_base/external_lib/com/fluidwebkit/fluidwebkit-*-sources.jar
+rm -rfv $WORKSPACE_HOME/FlowJob/flow-job-setup/docker/koekiebox/fluid_base/external_lib/com/fluidwebkit/fluidwebkit-*-javadoc.jar
+
+echo '[INFO] Updating WildFly-20'
+cp -fv $WORKSPACE_HOME/FlowJob/flow-job-setup/docker/koekiebox/fluid_base/external_lib/com/fluidbpmwebkit/fluidwebkit-*.jar $WF_HOME/modules/com/fluidbpmwebkit/main/
 
 echo '[INFO] *** *** *** ***'
 echo '[INFO] * DONE. Date: '$(date)
