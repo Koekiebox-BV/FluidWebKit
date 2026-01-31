f9d88a622d20206924bd0c2117ec32fd0be71e72
jasonbruwerstan <jason@koekiebox.com>
2026-01-31 15:54:38 +0100

    Jakarta: Migrate to PrimeFaces 15, Jakarta 10, JDK 11 and WildFly 34.0.1.Final. Bump to latest PrimeFaces.

--- src/main/java/com/fluidbpm/fluidwebkit/backing/bean/login/ABaseLoginBean.java
+++ src/main/java/com/fluidbpm/fluidwebkit/backing/bean/login/ABaseLoginBean.java
@@ -40,194 +40,194 @@ import java.util.Properties;
  * @since 1.0
  */
 public abstract class ABaseLoginBean extends ABaseManagedBean {
-	private String inputUsername = null;
-	private String inputPassword = null;
+    private String inputUsername = null;
+    private String inputPassword = null;
 
-	private static final int MAX_LOGIN_MSG = 50;
+    private static final int MAX_LOGIN_MSG = 50;
 
-	@Inject
-	private LoggedInUsersBean loggedInUsersBean;
+    @Inject
+    private LoggedInUsersBean loggedInUsersBean;
 
-	/**
-	 * action="#{loginBean.actionLogin}"
-	 * @return navigation {@code dashboard}.
-	 */
-	public String actionLogin() {
-		Properties existingProps = null;
+    /**
+     * action="#{loginBean.actionLogin}"
+     * @return navigation {@code dashboard}.
+     */
+    public String actionLogin() {
+        Properties existingProps = null;
 
-		this.getLogger().debug("Login attempt ["+ this.getInputUsername()+":"+ UtilGlobal.getConfigURLFromSystemProperty(existingProps)+"]");
+        this.getLogger().debug("Login attempt ["+ this.getInputUsername()+":"+ UtilGlobal.getConfigURLFromSystemProperty(existingProps)+"]");
 
-		//Let's confirm username and password is set first...
-		if (this.getInputUsername() == null ||
-				this.getInputUsername().trim().isEmpty() ||
-				this.getInputPassword() == null || this.getInputPassword().trim().isEmpty()){
+        //Let's confirm username and password is set first...
+        if (this.getInputUsername() == null ||
+                this.getInputUsername().trim().isEmpty() ||
+                this.getInputPassword() == null || this.getInputPassword().trim().isEmpty()){
 
-			FacesMessage fMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-					"Invalid.", "Username and Password required.");
-			FacesContext.getCurrentInstance().addMessage(null, fMsg);
-			return null;
-		}
+            FacesMessage fMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Invalid.", "Username and Password required.");
+            FacesContext.getCurrentInstance().addMessage(null, fMsg);
+            return null;
+        }
 
-		User user = new User();
-		user.setUsername(this.getInputUsername());
-		LoginClient loginClient = new LoginClient(UtilGlobal.getConfigURLFromSystemProperty(existingProps));
-		try {
-			if (!this.isConfigUserLoginSuccess()) {
-				throw new ClientDashboardException(
-						"'Config User' not logged in. Please check logs!",
-						ClientDashboardException.ErrorCode.ILLEGAL_STATE);
-			}
+        User user = new User();
+        user.setUsername(this.getInputUsername());
+        LoginClient loginClient = new LoginClient(UtilGlobal.getConfigURLFromSystemProperty(existingProps));
+        try {
+            if (!this.isConfigUserLoginSuccess()) {
+                throw new ClientDashboardException(
+                        "'Config User' not logged in. Please check logs!",
+                        ClientDashboardException.ErrorCode.ILLEGAL_STATE);
+            }
 
-			//App Request Token ...
-			this.getLogger().debug("Performing login action.");
+            //App Request Token ...
+            this.getLogger().debug("Performing login action.");
 
-			final String serviceTicket;
-			if (Globals.isConfigBasicAuthFromSystemProperty()) {
-				this.getLogger().debug("Login using [BASIC] authentication.");
-				User loginBasicUser = loginClient.loginBasic(
-						this.getInputUsername(), this.getInputPassword());
-				serviceTicket = loginBasicUser.getServiceTicket();
-				user.setRoles(loginBasicUser.getRoles());
-			} else {
-				this.getLogger().debug("Login using [TOKEN] authentication.");
-				AppRequestToken appReqToken = loginClient.login(
-						this.getInputUsername(), this.getInputPassword());
-				serviceTicket = appReqToken.getServiceTicket();
-				user.setRoles(Role.convertToObjects(appReqToken.getRoleString()));
-			}
+            final String serviceTicket;
+            if (Globals.isConfigBasicAuthFromSystemProperty()) {
+                this.getLogger().debug("Login using [BASIC] authentication.");
+                User loginBasicUser = loginClient.loginBasic(
+                        this.getInputUsername(), this.getInputPassword());
+                serviceTicket = loginBasicUser.getServiceTicket();
+                user.setRoles(loginBasicUser.getRoles());
+            } else {
+                this.getLogger().debug("Login using [TOKEN] authentication.");
+                AppRequestToken appReqToken = loginClient.login(
+                        this.getInputUsername(), this.getInputPassword());
+                serviceTicket = appReqToken.getServiceTicket();
+                user.setRoles(Role.convertToObjects(appReqToken.getRoleString()));
+            }
 
-			// Mark the user as logged in:
-			this.loggedInUsersBean.loginUser(this.getInputUsername());
+            // Mark the user as logged in:
+            this.loggedInUsersBean.loginUser(this.getInputUsername());
 
-			//We have a valid login here...
-			user.setServiceTicket(serviceTicket);
-			this.bindFluidClientDSToSession(serviceTicket);
+            //We have a valid login here...
+            user.setServiceTicket(serviceTicket);
+            this.bindFluidClientDSToSession(serviceTicket);
 
-			//Bind the config for the Fluid DS User...
-			this.bindConfigFluidClientDS();
+            //Bind the config for the Fluid DS User...
+            this.bindConfigFluidClientDS();
 
-			this.getLogger().debug("User logged in. Retrieving logged in user info.");
+            this.getLogger().debug("User logged in. Retrieving logged in user info.");
 
-			//Get logged in user info...
-			User loggedInUserInfo = this.getFluidClientDS().getUserClient().getLoggedInUserInformation();
+            //Get logged in user info...
+            User loggedInUserInfo = this.getFluidClientDS().getUserClient().getLoggedInUserInformation();
 
-			//Set the User Id...
-			user.setDateFormat(loggedInUserInfo.getDateFormat());
-			user.setTimeFormat(loggedInUserInfo.getTimeFormat());
-			user.setId(loggedInUserInfo.getId());
+            //Set the User Id...
+            user.setDateFormat(loggedInUserInfo.getDateFormat());
+            user.setTimeFormat(loggedInUserInfo.getTimeFormat());
+            user.setId(loggedInUserInfo.getId());
 
-			this.getLogger().debug("Retrieving user field listing.");
-			try {
-				UserFieldListing fieldListing = this.getFluidClientDSConfig().getUserClient().getAllUserFieldValuesByUser(user);
-				if (!fieldListing.isListingEmpty()) {
-					user.setUserFields(fieldListing.getListing());
-				}
-			} catch (FluidClientException fle) {
-				this.getLogger().error("Unable to get user fields for "+ loggedInUserInfo.getUsername()+". "+fle.getMessage(),fle);
-			}
-		} catch (FluidClientException fce) {
-			this.getLogger().error(fce.getMessage(), fce);
-			if (FluidClientException.ErrorCode.LOGIN_FAILURE == fce.getErrorCode()) {
-				this.getFluidClientDSConfig().getUserClient().incrementInvalidLoginForUser(user);
-				FacesMessage fMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Failed to Login.", "");
-				FacesContext.getCurrentInstance().addMessage(null, fMsg);
-				return null;
-			}
+            this.getLogger().debug("Retrieving user field listing.");
+            try {
+                UserFieldListing fieldListing = this.getFluidClientDSConfig().getUserClient().getAllUserFieldValuesByUser(user);
+                if (!fieldListing.isListingEmpty()) {
+                    user.setUserFields(fieldListing.getListing());
+                }
+            } catch (FluidClientException fle) {
+                this.getLogger().error("Unable to get user fields for "+ loggedInUserInfo.getUsername()+". "+fle.getMessage(),fle);
+            }
+        } catch (FluidClientException fce) {
+            this.getLogger().error(fce.getMessage(), fce);
+            if (FluidClientException.ErrorCode.LOGIN_FAILURE == fce.getErrorCode()) {
+                this.getFluidClientDSConfig().getUserClient().incrementInvalidLoginForUser(user);
+                FacesMessage fMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Failed to Login.", "");
+                FacesContext.getCurrentInstance().addMessage(null, fMsg);
+                return null;
+            }
 
-			FacesMessage fMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-					"Failed to Login. ", this.shortenMessage(fce.getMessage(), MAX_LOGIN_MSG));
-			FacesContext.getCurrentInstance().addMessage(null, fMsg);
-			return null;
-		} catch (ClientDashboardException dashExcept) {
-			this.getLogger().error(dashExcept.getMessage(), dashExcept);
+            FacesMessage fMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Failed to Login. ", this.shortenMessage(fce.getMessage(), MAX_LOGIN_MSG));
+            FacesContext.getCurrentInstance().addMessage(null, fMsg);
+            return null;
+        } catch (ClientDashboardException dashExcept) {
+            this.getLogger().error(dashExcept.getMessage(), dashExcept);
 
-			if (dashExcept.getErrorCode() == ClientDashboardException.ErrorCode.IP_CHANGED) {
-				FacesMessage fMsg = new FacesMessage(FacesMessage.SEVERITY_WARN,
-						"Existing Login on another IP address. ", this.shortenMessage(dashExcept.getMessage(), MAX_LOGIN_MSG));
-				FacesContext.getCurrentInstance().addMessage(null, fMsg);
-			} else {
-				FacesMessage fMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-						"Failed to Login. ", this.shortenMessage(dashExcept.getMessage(), MAX_LOGIN_MSG));
-				FacesContext.getCurrentInstance().addMessage(null, fMsg);
-			}
-			return null;
-		} finally {//Close the login after done...
-			loginClient.closeAndClean();
-		}
+            if (dashExcept.getErrorCode() == ClientDashboardException.ErrorCode.IP_CHANGED) {
+                FacesMessage fMsg = new FacesMessage(FacesMessage.SEVERITY_WARN,
+                        "Existing Login on another IP address. ", this.shortenMessage(dashExcept.getMessage(), MAX_LOGIN_MSG));
+                FacesContext.getCurrentInstance().addMessage(null, fMsg);
+            } else {
+                FacesMessage fMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                        "Failed to Login. ", this.shortenMessage(dashExcept.getMessage(), MAX_LOGIN_MSG));
+                FacesContext.getCurrentInstance().addMessage(null, fMsg);
+            }
+            return null;
+        } finally {//Close the login after done...
+            loginClient.closeAndClean();
+        }
 
-		this.getLogger().debug("Adding user to session.");
-		//Only put the user if success...
-		Map<String, Object> sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
-		sessionMap.put(SessionVariable.USER, user);
+        this.getLogger().debug("Adding user to session.");
+        //Only put the user if success...
+        Map<String, Object> sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+        sessionMap.put(SessionVariable.USER, user);
 
-		return Outcome.DASHBOARD;
-	}
+        return Outcome.DASHBOARD;
+    }
 
-	private String shortenMessage(String message, int maxLength) {
-		if (message == null) return message;
-		if (message.length() > maxLength) return String.format("%s ...", message.substring(0, maxLength));
-		return message;
-	}
+    private String shortenMessage(String message, int maxLength) {
+        if (message == null) return message;
+        if (message.length() > maxLength) return String.format("%s ...", message.substring(0, maxLength));
+        return message;
+    }
 
-	/**
-	 * Log a user out of the Dashboard.
-	 *
-	 * @return navigation {@code dashboard}
-	 */
-	public String actionLogout() {
-		FacesContext facesContext = FacesContext.getCurrentInstance();
+    /**
+     * Log a user out of the Dashboard.
+     *
+     * @return navigation {@code dashboard}
+     */
+    public String actionLogout() {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
 
-		HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(false);
-		if (session != null) session.invalidate();
+        HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(false);
+        if (session != null) session.invalidate();
 
-		return this.getLogoutOutcomePage();
-	}
+        return this.getLogoutOutcomePage();
+    }
 
-	/**
-	 * Confirm whether the Fluid config user was logged in successfully.
-	 * @return {@code true} if logged in successfully. Otherwise {@code false}.
-	 */
-	public abstract boolean isConfigUserLoginSuccess();
+    /**
+     * Confirm whether the Fluid config user was logged in successfully.
+     * @return {@code true} if logged in successfully. Otherwise {@code false}.
+     */
+    public abstract boolean isConfigUserLoginSuccess();
 
-	/**
-	 * The JSF {@code outcome} for when a logout action is performed.
-	 * @return JSF outcome.
-	 */
-	public abstract String getLogoutOutcomePage();
+    /**
+     * The JSF {@code outcome} for when a logout action is performed.
+     * @return JSF outcome.
+     */
+    public abstract String getLogoutOutcomePage();
 
-	/**
-	 * The password provided by the user.
-	 *
-	 * @return User password.
-	 */
-	public String getInputPassword() {
-		return this.inputPassword;
-	}
+    /**
+     * The password provided by the user.
+     *
+     * @return User password.
+     */
+    public String getInputPassword() {
+        return this.inputPassword;
+    }
 
-	/**
-	 * The password provided by the user.
-	 *
-	 * @param inputPasswordParam The JSF bean mapping for password.
-	 */
-	public void setInputPassword(String inputPasswordParam) {
-		this.inputPassword = inputPasswordParam;
-	}
+    /**
+     * The password provided by the user.
+     *
+     * @param inputPasswordParam The JSF bean mapping for password.
+     */
+    public void setInputPassword(String inputPasswordParam) {
+        this.inputPassword = inputPasswordParam;
+    }
 
-	/**
-	 * The username provided by the user.
-	 *
-	 * @return The JSF bean mapping for username.
-	 */
-	public String getInputUsername() {
-		return this.inputUsername;
-	}
+    /**
+     * The username provided by the user.
+     *
+     * @return The JSF bean mapping for username.
+     */
+    public String getInputUsername() {
+        return this.inputUsername;
+    }
 
-	/**
-	 * The username provided by the user.
-	 * 
-	 * @param inputUsernameParam  The JSF bean mapping for username.
-	 */
-	public void setInputUsername(String inputUsernameParam) {
-		this.inputUsername = inputUsernameParam;
-	}
+    /**
+     * The username provided by the user.
+     *
+     * @param inputUsernameParam  The JSF bean mapping for username.
+     */
+    public void setInputUsername(String inputUsernameParam) {
+        this.inputUsername = inputUsernameParam;
+    }
 }
